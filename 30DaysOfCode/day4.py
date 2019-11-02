9216e9966e179b4e9962bccbd2aef5122c650a11
arwildo <arwildo@gmail.com>
2019-11-02 13:52:06 +0800

    fix day4

--- 30DaysOfCode/day4.py
+++ 30DaysOfCode/day4.py
@@ -1,22 +1,26 @@
 #!/bin/python3
 
-class Person(int):
-    
-#    def __init__(self):
-#        self.age = ()
-#
-#    if self.age < 0:
-#        print("Age is not valid, setting age to 0")
-#    elif self.age > 0:
-#        initialAge = self.age
+class Person(object):
+    def __init__(self, initialAge):
+        self.age = 0 
+        self.initialAge = initialAge
 
-    def yearPasses(initialAge):
-        initialAge += 1
+        if self.initialAge < 0:
+            print("Age is not valid, setting age to 0")
+        elif self.initialAge > 0:
+            self.age = initialAge
 
-    def amIOld(initialAge):
-        if initialAge < 13:
+    def yearPasses(self):
+        self.age += 1
+        print(self.age)
+
+    def amIOld(self):
+        if self.age < 13:
             print("You are young.")
-        elif initialAge >= 13 and initialAge < 18:
+        elif self.age >= 13 and self.age < 18:
             print("You are a teenager.")
         else:
             print("You are old.")
+
+
+Person(int(input())).yearPasses()
