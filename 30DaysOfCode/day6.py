68c328cd696ac29228ab903978745665999c36bb
arwildo <arwildo@gmail.com>
2019-11-05 20:51:06 +0800

    fix day 6 & day 7

--- 30DaysOfCode/day6.py
+++ 30DaysOfCode/day6.py
@@ -1,1 +1,26 @@
 #!/bin/python3
+
+def evenStr(s):
+    l = len(s)
+    result = ""
+
+    for i in range(0, l, 2):
+        result += s[i]
+    return result
+
+def oddStr(s):
+    l = len(s)
+    result = ""
+
+    for i in range (1, l, 2):
+        result += s[i]
+    return result
+
+
+T = int(input())
+
+for r in range(0, T):
+    s = input()
+    print(evenStr(s) + ' ' + oddStr(s))
+
+    
--- 30DaysOfCode/day7.py
+++ 30DaysOfCode/day7.py
@@ -1,27 +1,16 @@
 #!/bin/python3
 
-#def rev(arr):
-#    for i in arr:
-#        return arr[len-1]
-#
-#
-#if __name__ == '__main__':
-#    #n = int(input())
-#    #print(n)
-#    arr = list(map(int, input().rstrip().split()))
-#    revArr = rev(arr)
-#    print(arr)
+if __name__ == '__main__':
+    n = int(input())
 
-def revArr(test):
-    result = []
-    count = len(test)
-    for i in test:
-        result.append(test[count-1])
-        count -= 1
-    print(result)
-
-
-test = list(map(int, input().rstrip().split()))
-print(test)
-revArr(test)
+    arr = list(map(int, input().rstrip().split()))
+    res = ""
     
+    if n == len(arr):
+        for i in range(len(arr)-1, -1, -1):
+            res += str(arr[i]) + " "
+    else:
+        print("Error: incorrect array size.")
+
+
+    print(res)
